8950bace6c85505301d010fc9663023e5bd98ea1
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-28 12:04:17 -0300

    First commit

---
+++ src/main/java/br/com/dino/planetapi/SwPlanetApiTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.dino.planetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPlanetApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwPlanetApiTestApplication.class, args);
+    }
+
+}
